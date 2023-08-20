5d59245482a4c6c592e01862b4c6a3909d7d1f70
saehim <saeilee.vxv@gmail.com>
2023-08-20 16:17:49 +0900

    JPA: practice project init

---
+++ jpa-orm/src/main/java/com/example/jpaorm/JpaOrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaOrmApplication.class, args);
+    }
+
+}
---
+++ jpa-orm/src/test/java/com/example/jpaorm/JpaOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaOrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
