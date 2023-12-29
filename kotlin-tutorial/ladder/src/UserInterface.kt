d2093ed7da543bea2b663c68dbfc1a8a9594e220
saehim <saeilee.vxv@gmail.com>
2023-12-30 03:09:14 +0900

    Study: kotlin Ladder 연습 - 2단계 사용자 이름 입력받도록 수정

--- kotlin-tutorial/ladder/src/UserInterface.kt
+++ kotlin-tutorial/ladder/src/UserInterface.kt
@@ -2,9 +2,11 @@ package src
 
 class UserInterface {
 
-    fun getPlayerNumber() : Int {
-        println("참여할 사람은 몇 명인가요?")
-        return readLine()!!.toInt() // !! : null이 아님을 보장. null이면 NPE 발생
+    fun getPlayers() : Players {
+        println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)")
+        val input = readLine()!! // !! : null이 아님을 보장. null이면 NPE 발생
+        val names = input.split(",").map { it.trim() }
+        return Players(names.map { Player(it) })
     }
 
     fun getLadderLayerNumber() : Int {
