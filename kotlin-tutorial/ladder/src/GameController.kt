59580aa390d7a0f9aebc6c72f72cc3bfbbb8ca53
saehim <saeilee.vxv@gmail.com>
2023-12-30 03:09:47 +0900

    Study: kotlin Ladder 연습 - 2단계 `Player` 객체 만들기

--- kotlin-tutorial/ladder/src/GameController.kt
+++ kotlin-tutorial/ladder/src/GameController.kt
@@ -1,12 +1,14 @@
+package src
+
 import src.*
 
 fun main() {
     // 1단계 : 유저 입력 받기
     val ui = UserInterface()
-    val playerNumber = ui.getPlayerNumber()
+    val players = ui.getPlayers()
     val ladderLayer = ui.getLadderLayerNumber()
 
     // 2단계 : 사다리 초기화
-    val ladder = Ladder(ladderLayer, playerNumber)
+    val ladder = Ladder(ladderLayer, players)
     ladder.printLadder()
 }
---
+++ kotlin-tutorial/ladder/src/Players.kt
@@ -0,0 +1,26 @@
+package src
+
+data class Players(val players: List<Player>) {
+
+    init {
+        require(players.size >= 2) { "플레이어는 2명 이상이어야 합니다." }
+    }
+
+    fun printPlayers() {
+        print("\t")
+        players.forEach { print("${it.name}\t") }
+        println()
+    }
+
+    fun getSize() : Int {
+        return players.size
+    }
+}
+
+data class Player(val name: String) {
+    init {
+        require(name.length >= 1) { "이름은 1글자 이상이어야 합니다." }
+        require(name.length <= 5) { "이름은 5글자 이하여야 합니다." }
+        require(name.isNotBlank()) { "이름은 공백이 아니어야 합니다." }
+    }
+}
